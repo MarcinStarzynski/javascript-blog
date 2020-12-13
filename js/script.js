a3a45dd064abb3dba55258e2fb5fcd68a6ddfcc1
Marcin Starzynski <gienekmsms@gmail.com>
2020-12-13 15:56:28 +0100

    Add Active Functions

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@
     //console.log('links:', links);
   //});
   const titleClickHandler = function(){
+        event.preventDefault();
         const clickedElement = this;
         console.log('Link was clicked!');
 
@@ -23,10 +24,14 @@
             activeArticle.classList.remove('active');
         }
   /* get 'href' attribute from the clicked link */
-
+        const articleSelector = clickedElement.getAttribute('href');
+        console.log(articleSelector);
   /* find the correct article using the selector (value of 'href' attribute) */
-
+        const targetArticle = document.querySelector(articleSelector);
+        console.log(targetArticle);
   /* add class 'active' to the correct article */
+        targetArticle.classList.add('active');
+
     }
         const links = document.querySelectorAll('.titles a');
   
